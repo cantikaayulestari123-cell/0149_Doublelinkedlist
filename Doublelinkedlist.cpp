68de9458352fa73c8eeadd3623b58c82638c9eae
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-30 13:26:34 +0700

    menambahkan fungsi revtraverse

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -143,7 +143,35 @@ void hapus()
             currentNode = currentNode->next;
             i++;
         }
-}
+    }
+
+     void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        Node *currentNode = START;
+        int i = 0;
+
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        cout << "\nRecords in descending order of roll number are:\n";
+
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
+
 
         
 
